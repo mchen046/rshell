200c2c506d042e8e9eb52ccae75266e50f008114
Michael Chen <mchen046@hammer.cs.ucr.edu>
2015-04-12 17:10:10 -0700

    proper parsing

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,21 @@
 #include <string.h>
 #include <string>
 #include <vector>
+#include <cstdlib>
+#include <stdio.h>
+#include <boost/tokenizer.hpp>
+#include <boost/token_iterator.hpp>
 
 using namespace std;
+using namespace boost;
 
 char** parseString(string cmd);
 
-void checkExitCmd(char* cmdstrtok[]){ //exit command
+void checkExitCmd(char* cmdText){ //exit command
 	char exitText[] = {'e','x', 'i', 't'};
 	bool same = true;
-	for(int i = 0; cmdstrtok[0][i]!='\0' && same; i++){
-		if(cmdstrtok[0][i]!=exitText[i])
+	for(int i = 0; cmdText[i]!='\0' && same; i++){
+		if(cmdText[i]!=exitText[i])
 			same = false;
 	}
 	if(same)
@@ -28,44 +33,63 @@ int main(){
 		getline(cin, cmd);
 
 		//convert string to char[]
-		char cmdArray[cmd.size()];
-		for(int i=0; i<cmd.size(); i++){
+		char *cmdArray = new char[cmd.size()];
+		for(int i=0; i<static_cast<int>(cmd.size()); i++){
 			cmdArray[i] = cmd.at(i);
 		}
+		//cout << "cmdArray: " << cmdArray << endl;
 		
-		//strtok (possible bug reading in '@' on the fifth token)
-		char *cmdstrtok[cmd.size()];
-		char *token = strtok(cmdArray, " ");
-		for(int i = 0; token!='\0'; i++){
-			cout << "token: " << token << endl;
-			cmdstrtok[i] = token;
-			token = strtok(NULL, " ");
-			if(token == '\0'){
-				cmdstrtok[i+1] = token;
-			}
-		}
+		bool cmdLineDone = false;
+		char *cmdLine = strtok(cmdArray, ";"); //parsing ";"
+		while(!cmdLineDone){
+			//cout << "cmdLine: " << cmdLine << endl;
+			
+			//parsing " "
+			string str = static_cast<string>(cmdLine);
+			char_separator<char> delim(" ");
+			tokenizer< char_separator<char> > mytok(str, delim);
 
-		checkExitCmd(cmdstrtok);
+			//creating cmdLinePart
+			char **cmdLinePart = new char*[cmd.size()];
+			int i = 0;
+			for(tokenizer <char_separator<char> >::iterator it = mytok.begin(); it!=mytok.end(); it++){
+				char *token = new char[cmd.size()];
+				string cmdString = static_cast<string>(*it);
+				for(unsigned int j = 0; j!=cmdString.size(); j++){
+					token[j] = cmdString[j];
+				}
+				cmdLinePart[i] = token;
+				i++;
+			}
 
-		//process spawning
-		int pid = fork();
-		cout << "pid: " << pid << endl;
-		if(pid == -1){ //error
-			perror("fork() error");
-			exit(1);
-		}
-		else if(pid == 0){ //child process
-			cout << "in child process\n";
-			if(execvp(cmdstrtok[0], cmdstrtok) == -1){
-				perror("error in execvp");
+			cmdLine = strtok(NULL, ";"); //parse ";"
+			if(cmdLine==NULL){
+				cmdLineDone = true;
 			}
-			exit(1);
-		}
-		else if(pid > 0){ //parent process
-			if(wait(0) == -1){
-				perror("error in wait()");
+			checkExitCmd(cmdLinePart[0]); //check for exit command
+
+			//process spawning
+			int pid = fork();
+			//cout << "pid: " << pid << endl;
+			if(pid == -1){ //error
+				perror("fork() error");
+				exit(1);
+			}
+			else if(pid == 0){ //child process
+				//cout << "in child process\n";
+				if(execvp(cmdLinePart[0], cmdLinePart) == -1){
+					perror("error in execvp");
+				}
+				exit(1);
+			}
+			else if(pid > 0){ //parent process
+				if(wait(0) == -1){
+					perror("error in wait()");
+				}
 			}
 		}
+		//delete[] *cmdstrtok; //deallocate cmdstrtok
+		//delete[] cmdArray;
 	}
 	return 0;
 }
